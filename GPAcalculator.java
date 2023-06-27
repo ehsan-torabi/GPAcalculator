a2a2c897bee4b7398855d86faeece555d8992be5
ehsan-torabi <torabi782@gmail.com>
2023-06-27 16:48:06 +0330

    Exceptin handling in getting courese section

--- GPAcalculator.java
+++ GPAcalculator.java
@@ -7,30 +7,34 @@ public class GPAcalculator {
         System.out.print("Please Enter Count OF your courses: ");
         int numOfCourses = scanner.nextInt();
         Course[] courses = new Course[numOfCourses];
-        double totalUnits = 0;
-
         for (int i = 0; i < numOfCourses; i++) {
-            String[] valStrings = new String[3];
-            System.out.printf(Locale.ENGLISH, "Please enter name,unit,score in same format of your course %d: %n",
-                    i + 1);
-            String values = scanner.next();
-            valStrings = values.split(",");
-            int unit = Integer.parseInt(valStrings[1]);
-            double score = Double.parseDouble(valStrings[2]);
-            String name = valStrings[0];
-            courses[i] = new Course(name, unit, score);
-            totalUnits += unit;
-            System.out.println("------------------------------------------------------");
+            try {
+                String[] valStrings = new String[3];
+                System.out.printf(Locale.ENGLISH, "Please enter name,unit,score in same format of your course %d: %n",
+                        i + 1);
+                String values = scanner.next();
+                valStrings = values.split(",");
+                int unit = Integer.parseInt(valStrings[1]);
+                double score = Double.parseDouble(valStrings[2]);
+                String name = valStrings[0];
+                courses[i] = new Course(name, unit, score);
+                System.out.println("------------------------------------------------------");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Please Enter to below format:\n name,unit,score");
+                i--;
+            }
         }
 
         scanner.close();
 
+        double totalUnits = 0;
         double totalScores = 0;
         System.out.println("\nName" + "\t" + "Unit" + "\t" + "Score");
         System.out.println("---------------------");
         for (Course course : courses) {
             System.out.println(course.getName() + " \t" + course.getUnit() + " \t" + course.getScore());
             totalScores += course.calculateScore();
+            totalUnits += course.getUnit();
         }
 
         double gpa = totalScores / totalUnits;
